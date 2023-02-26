a384290206843622690b91a4b46bd21a3a6c276f
liqiang0335 <liqiang0335@qq.com>
2023-02-26 11:02:54 +0800

    Fixed

--- src/handler/ReplaceModule.js
+++ src/handler/ReplaceModule.js
@@ -5,6 +5,7 @@ module.exports = async function () {
   const editor = vscode.window.activeTextEditor;
   const content = editor.document.lineAt(editor.selection.active.line).text;
 
+  // 匹配斜杠后面的单词
   let matchTarget = content.match(/\/(\w+)/);
   if (!matchTarget) {
     return vscode.window.showErrorMessage("No target found");
@@ -13,7 +14,7 @@ module.exports = async function () {
   if (/s$/.test(target)) {
     target = "{ }";
   }
-  const newContent = content.replace("module", target);
+  const newContent = content.replace(/(const|import)\s+\w+/, `$1 ${target}`);
   await vscode.commands.executeCommand("editor.action.deleteLines");
 
   const position = editor.selection.end;
