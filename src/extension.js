2b6c8c853df7add092ae9cfe17c9fbbc5bc5bc9e
liqiang0335 <liqiang0335@qq.com>
2023-08-16 19:06:53 +0800

    增加Frame信息替换

--- src/extension.js
+++ src/extension.js
@@ -3,6 +3,7 @@ const RemoveBlank = require("./handler/RemoveBlank");
 const SaveImage = require("./handler/SaveImage");
 const ReplaceModule = require("./handler/ReplaceModule");
 const CmdCaller = require("./handler/CmdCaller");
+const HandleClipboard = require("./handler/HandleClipboard");
 
 /**
  * activate
@@ -11,11 +12,19 @@ exports.activate = function (context) {
   const subscriptions = context.subscriptions;
   const commands = vscode.commands;
 
-  const CmdCallerItems = ["CopyPathMd5", "CopyRelativePages", "SplitBookJSON", "CreateBookJSON", "UserDefined", "MarkdownToHTML"];
+  const CmdCallerItems = [
+    "CopyPathMd5",
+    "CopyRelativePages", // 相对Pages路径
+    "SplitBookJSON",
+    "CreateBookJSON",
+    "UserDefined",
+    "MarkdownToHTML",
+  ];
   CmdCallerItems.forEach((item) => {
     subscriptions.push(commands.registerCommand(`liqiang.${item}`, (URI) => CmdCaller(URI, item)));
   });
 
+  subscriptions.push(commands.registerCommand("liqiang.HandleClipboard", HandleClipboard));
   subscriptions.push(commands.registerCommand("liqiang.ReplaceModule", ReplaceModule));
   subscriptions.push(commands.registerCommand("liqiang.SaveImage", SaveImage));
   subscriptions.push(commands.registerCommand("liqiang.removeBlank", RemoveBlank));
---
+++ src/handler/HandleClipboard.js
@@ -0,0 +1,57 @@
+const vscode = require("vscode");
+const fs = require("fs");
+
+module.exports = async function (URI) {
+  let filePath = URI?.fsPath || vscode.window.activeTextEditor.document.uri.path;
+  let clipboardContent = await vscode.env.clipboard.readText();
+  let fileContent = fs.readFileSync(filePath, "utf-8");
+  const ctx = {
+    filePath,
+    fileContent,
+  };
+
+  let contents = "";
+  try {
+    contents = JSON.parse(clipboardContent);
+    if (!contents) {
+      vscode.window.showErrorMessage("剪切板中数据为空");
+      return;
+    }
+    if (!contents?.copytype) {
+      vscode.window.showErrorMessage("未匹配到copytype");
+      return;
+    }
+    const { copytype, data } = contents;
+    if (!handlers[copytype]) {
+      vscode.window.showErrorMessage("未匹配到copytype对应的处理函数: " + copytype);
+      return;
+    }
+    handlers[copytype](data, ctx);
+  } catch (error) {
+    vscode.window.showErrorMessage("剪切板中的内容不是JSON格式");
+    return;
+  }
+};
+
+const handlers = {
+  Frame(data, ctx) {
+    if (!Array.isArray(data) || !data.length) {
+      vscode.window.showErrorMessage("data 数组为空");
+      return;
+    }
+    let { filePath, fileContent } = ctx;
+    for (let item of data) {
+      const { name, x, y, w, h } = item;
+      const size = `${x}-${y}-${w}-${h}`;
+      const reg = new RegExp(`<Frame\\s*name="${name}"\\s*size="(.+?)"\\s*>`);
+      const match = fileContent.match(reg);
+      if (!match) {
+        vscode.window.showErrorMessage(`未匹配到${name}对应的Frame`);
+        continue;
+      }
+      fileContent = fileContent.replace(reg, `<Frame name="${name}" size="${size}">`);
+    }
+    fs.writeFileSync(filePath, fileContent);
+    vscode.window.showInformationMessage("Frame处理完成");
+  },
+};
